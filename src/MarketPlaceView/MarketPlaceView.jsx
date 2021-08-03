f13d6a2339ebdc8055b5f749befc1587a47a65df
Kong Yang <kpyang2414@gmail.com>
2021-08-03 11:29:48 -0500

    mapping out products in marketplaceview

--- src/MarketPlaceView/MarketPlaceView.jsx
+++ src/MarketPlaceView/MarketPlaceView.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 function Marketplace() {
 
+    const allproducts = useSelector(store => store.productReducer);
+
+    useEffect({
+        dispatch: "FETCH_PRODUCTS"
+    }, []);
 
     return(
-        <p>This is the marketplace view</p>
+        <section>
+            <h1>This is the marketplace view</h1>
+            <article>
+                {allproducts.map(product => {
+                    return (
+                        <div key={product.id} >
+                            <img src={product.img_url} ></img>
+                            <h2>{product.name}</h2>
+                            <h3>Price: {product.price}</h3>
+                            <h4>Description: {product.description}</h4>
+                        </div>
+                    )
+                })}
+            </article>
+        </section>
     )
 }
 
