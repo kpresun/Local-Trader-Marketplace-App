10a2e22496c3f0fe14473a4715911299b2721bc0
Kong Yang <kpyang2414@gmail.com>
2021-10-13 22:01:51 -0500

    removed comments and logs

--- server/routes/bookmark.router.js
+++ server/routes/bookmark.router.js
@@ -6,11 +6,9 @@ const router = express.Router();
  * POST route: adds item to bookmark
  */
 router.post('/', (req, res) => {
-  console.log('--log-- The req.body is:', req.body);
-  console.log('--LOG-- what is the current users id?', req.user.id);
   const userId = req.user.id;
   const productId = req.body.id;
-  console.log(req.user.id);
+
   const query = `INSERT INTO "bookmark" ("user_id", "product_id")
                 VALUES ($1, $2);`;
     pool.query(query, [userId, productId])
@@ -18,7 +16,6 @@ router.post('/', (req, res) => {
       res.sendStatus(200);
     })
     .catch(error => {
-        console.log('--ERROR-- unable to return bookmark:', error);
         res.sendStatus(500);
     })
 });
@@ -28,7 +25,6 @@ router.post('/', (req, res) => {
  */
  router.get('/user/:id', (req, res) => {
    userId = req.params.id;
-   console.log('--LOG-- the params.id is:', userId);
     const query = `SELECT "bookmark".id, "bookmark".user_id, "bookmark".product_id, "product".image_url, "product".name, "product".price, "product".description, "category".category_type, "status".status_type			
     FROM "product" JOIN "bookmark"
     ON "product".id = "bookmark".product_id
@@ -38,11 +34,9 @@ router.post('/', (req, res) => {
     WHERE "bookmark".user_id = $1;`;
     pool.query(query, [userId])
     .then(dbResponse => {
-      console.log('--log-- router.get, successfully returned bookmarks:', dbResponse);
       res.send(dbResponse.rows);
     })
     .catch(error => {
-      console.log('--ERROR-- router.get, unable to return bookmarks:', error);
       res.sendStatus(500);
     })
   });
@@ -52,7 +46,6 @@ router.post('/', (req, res) => {
  */
  router.get('/detail/:id', (req, res) => {
    const productId = req.params.id;
-   console.log('--LOG-- router.get, req.params.id is:', productId);
   const query = `SELECT "bookmark".id, "product".user_id, "bookmark".product_id, "product".image_url, "product".name, "product".price, "product".description, "category".category_type, "status".status_type			
   FROM "product" JOIN "bookmark"
   ON "product".id = "bookmark".product_id
@@ -62,11 +55,9 @@ router.post('/', (req, res) => {
   WHERE "product".id = $1;`;
   pool.query(query, [productId])
   .then(dbResponse => {
-    console.log('--log-- router.get detail/:id, successfully returned single bookmark:', dbResponse.rows);
     res.send(dbResponse.rows[0]);
   })
   .catch(error => {
-    console.log('--ERROR-- router.get, detail/:id unable to return single bookmarks:', error);
     res.sendStatus(500);
   })
 });
@@ -77,14 +68,12 @@ router.post('/', (req, res) => {
  */
 router.delete('/:id', (req, res) => {
   const deleteId = req.params.id;
-  console.log('--LOG-- Inside router.delete, deleteId is:', deleteId );
   const query = `DELETE FROM "bookmark" WHERE "id" = $1;`;
   pool.query(query, [deleteId])
   .then( dbResponse => {
     res.sendStatus(200);
   })
   .catch( error => {
-    console.log('--ERROR-- Unable to delete ID from bookmark:', error);
     res.sendStatus(500);
   })
 });
